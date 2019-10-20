85f6f283dba6b43e989bd666a1bab9edc54095c6
grace-n-smith <56238686+grace-n-smith@users.noreply.github.com>
2019-10-20 14:50:18 -0400

    Test Script
    Made a test script with Marwa for up-and-down motion.

---
+++ AliceProject/Assets/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject cube;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        cube.transform.Translate(cube.transform.position.x, 10*Mathf.Sin(0.01f * Time.deltaTime), cube.transform.position.z, Space.World);
+    }
+}
